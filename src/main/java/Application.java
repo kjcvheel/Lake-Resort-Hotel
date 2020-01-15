4666c81174027e4df985f9bd2d8adda38789a496
Osaigbovo <osas.osaigbovo@capgemini.com>
2020-01-15 15:10:34 +0100

    test1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("test Osayuwamen");
+        System.out.println("test Osayuwamen");
+
+
     }
 }
