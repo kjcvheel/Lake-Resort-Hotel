b6874661184ed54f88222eadb648ec02c3e05240
Osaigbovo <osas.osaigbovo@capgemini.com>
2020-01-15 15:16:56 +0100

    ommitter: Osaigbovo <osas.osaigbovo@capgemini.com>

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,7 @@
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("test Osayuwamen");
-        System.out.println("test Osayuwamen");
-
+        System.out.println("test");
 
     }
 }
