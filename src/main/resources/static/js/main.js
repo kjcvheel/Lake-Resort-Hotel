04fd9f903a3c8403a86957a3ddfe3609f638b6e2
jgroot <joey.de.groot@hotmai.com>
2020-01-29 12:24:32 +0100

    Improved the layout of the page and added test functions for get and post mapping

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,22 +1,64 @@
 var api = "http://localhost:8080/guests";
 
 function getData() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {}
-    }
+    console.log("getting data...");
+
+    $.ajax({
+        url: "http://localhost:8080/api/guests/test",
+        type: "get",
+        success: function(result) {
+            console.log("This is the data2: " + result);
+        }
+    });
 }
 
-function sendData() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let myObj = JSON.parse(this.responseText);
-            // Set inner html to this.
-        }
+function postData() {
+    console.log("sending data...")
+
+    let guestObj = {
+        firstName: $("#fname").val(),
+        lastName: $("#lname").val()
     };
-    let form = "#guest-information-form";
-    let guest = JSON.stringify($(form).serializeArray());
-    xhttp.open("POST", "application/json", true);
-    xhttp.send(guest);
+
+    let jsonObj = JSON.stringify(guestObj);
+
+    $.ajax({
+        url: "http://localhost:8080/api/guests/test",
+        type: "post",
+        data: jsonObj,
+        contentType: "application/json",
+        success: function(result) {
+            console.log("This was posted " + result.firstName + " " + result.lastName);
+        }
+    });
+
+}
+
+$(document).ready(function() {
+    console.log("Fill drowndown options.");
+    fillBirthdayDropdowns();
+});
+
+$("#getButton").click(function() {
+    getData();
+});
+
+$("#postButton").click(function() {
+    postData();
+});
+
+function fillBirthdayDropdowns() {
+    let i;
+    for (i = 1; i <= 31; i++) {
+        let option = "<option value='" + i + "'>" + i + "</option>";
+        $(".daySelect").append($(option));
+    }
+    for (i = 1; i <= 12; i++) {
+        let option = "<option value='" + i + "'>" + i + "</option>";
+        $(".monthSelect").append($(option));
+    }
+    for (i = new Date().getFullYear(); i >= 1900; i--) {
+        let option = "<option value='" + i + "'>" + i + "</option>";
+        $(".yearSelect").append($(option));
+    }
 }
