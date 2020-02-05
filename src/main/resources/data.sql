3268f2f2e26f37404b280704aded1794ed75c5de
kjcvheel <kevinvanheel94@hotmail.com>
2020-02-05 09:04:29 +0100

    two other users, working

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
-INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('1', '0', 'false', 'test', 'Chimpansee', 200 ,'false', 'Available', 'Single');
-INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('2', '0', 'false', 'test', 'Gorilla', 300, 'false', 'Unavailable', 'Double');
-INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('4', '0', 'false', 'test', 'Baboon', 400, 'false', 'Available', 'Double');
-INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('6', '1', 'true', 'test', 'The Orang-Utan', 500, 'true', 'Available',  'Penthouse');
+INSERT INTO ROOM (adult, children, disabled, image, name, price, smoking, status, type) VALUES ('1', '0', 'false', 'test', 'Chimpansee', 200 ,'false', 'Available', 'Single');
+INSERT INTO ROOM (adult, children, disabled, image, name, price, smoking, status, type) VALUES ('2', '0', 'false', 'test', 'Gorilla', 300, 'false', 'Unavailable', 'Double');
+INSERT INTO ROOM (adult, children, disabled, image, name, price, smoking, status, type) VALUES ('4', '0', 'false', 'test', 'Baboon', 400, 'false', 'Available', 'Double');
+INSERT INTO ROOM (adult, children, disabled, image, name, price, smoking, status, type) VALUES ('6', '1', 'true', 'test', 'The Orang-Utan', 500, 'true', 'Available',  'Penthouse');
 
 INSERT INTO GUEST(first_name, last_name, email, password, creditcard) VALUES ('kevin', 'van heel', 'kevinvanheel@capgemini.com', 'monkeys', 53435);
+INSERT INTO GUEST(first_name, last_name, email, password, creditcard) VALUES ('joey', 'de groot', 'joey-de-groot@capgemini.com', 'bananas', 1231);
+INSERT INTO GUEST(first_name, last_name, email, password, creditcard) VALUES ('nootje', 'wittink', 'noortje-wittink@capgemini.com', 'grapes', 31231);
