dbca53f47b134a3c97f49b95db0b404012afe570
kjcvheel <kevinvanheel94@hotmail.com>
2020-02-05 08:21:34 +0100

    check if changes are visible as I pushed to local branch

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO ROOM (adult, children, disabled, IMAGE, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('1', '0', 'false', 'test', 'Chimpansee', 200 ,'false', 'Available', 'Single');
-INSERT INTO ROOM (adult, children, disabled, IMAGE, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('2', '0', 'false', 'test', 'Gorilla', 300, 'false', 'Unavailable', 'Double');
-INSERT INTO ROOM (adult, children, disabled, IMAGE, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('4', '0', 'false', 'test', 'Baboon', 400, 'false', 'Available', 'Double');
-INSERT INTO ROOM (adult, children, disabled, IMAGE, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('6', '1', 'true', 'test', 'The Orang-Utan', 500, 'true', 'Available',  'Penthouse');
+INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('1', '0', 'false', 'test', 'Chimpansee', 200 ,'false', 'Available', 'Single');
+INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('2', '0', 'false', 'test', 'Gorilla', 300, 'false', 'Unavailable', 'Double');
+INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('4', '0', 'false', 'test', 'Baboon', 400, 'false', 'Available', 'Double');
+INSERT INTO ROOM (adult, children, disabled, image, NAME, PRICE, SMOKING, STATUS, TYPE) VALUES ('6', '1', 'true', 'test', 'The Orang-Utan', 500, 'true', 'Available',  'Penthouse');
 
 INSERT INTO GUEST(first_name, last_name, email, password, creditcard) VALUES ('kevin', 'van heel', 'kevinvanheel@capgemini.com', 'monkeys', 53435);
